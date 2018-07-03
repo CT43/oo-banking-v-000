82b6f605e7612da912f3750ae97035e29b4666d7
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 18:20:43 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,9 +2,9 @@ class Transfer
   attr_reader :sender, :reciever, :amount
 
   def initialize(sender, reciever, amount)
-    @sender = sender 
-    @reciever = reciever 
-    @amount = amount 
-  end 
-  
+    @sender = sender
+    @reciever = reciever
+    @amount = amount
+  end
+
 end
